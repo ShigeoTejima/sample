fbf9fdaacc990b369a20e023e82e0c4823cd6768
pekokun <pekokun@gmail.com>
2016-04-11 10:06:43 +0900

    add dummy testcase

---
+++ src/test/java/sample/FooTest.java
@@ -0,0 +1,12 @@
+package sample;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class FooTest {
+
+    @Test
+    public void test_success_always() {
+        assertTrue("This testcase is success always.", true);
+    }
+}
